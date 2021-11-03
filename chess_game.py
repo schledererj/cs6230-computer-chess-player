87f5248213cdaec6a7ecdd2221079e985913246b
Jack Schlederer <schlederer3@gmail.com>
2021-11-03 01:04:37 -0500

    Fix whitespace

--- chess_game.py
+++ chess_game.py
@@ -1,14 +1,12 @@
 import chess
 
+
 class ChessGame:
-	def __init__(self):
-		self.board = chess.Board()
-		
+    def __init__(self):
+        self.board = chess.Board()
 
-	def reset(self):
-		pass
+    def reset(self):
+        pass
 
-
-	def play_turn(self):
-		pass
-
+    def play_turn(self):
+        pass
